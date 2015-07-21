c870ca1716da9c36317926a7e7765d8613bddf66
Arsenyk Ustaris <arsenyku@users.noreply.github.com>
2015-07-20 21:05:35 -0700

    Initial Commit

---
+++ LargestNumberAssignment/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LargestNumberAssignment
+//
+//  Created by asu on 2015-07-20.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
